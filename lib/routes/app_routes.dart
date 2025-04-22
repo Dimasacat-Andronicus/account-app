4dc5af532bf65bd2992190dc0ec886a3489372a8
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-04-22 16:11:08 +0800

    update import path for home_view

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,7 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:hive/hive.dart';
 import '../auth_bloc/auth_login_view.dart';
 import '../auth_bloc/auth_register_view.dart';
-import '../home_bloc/home_view.dart';
+import '../home_bloc/view/home_view.dart';
 
 class AppRoutes {
   static const String login = '/login';
